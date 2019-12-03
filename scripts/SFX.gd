f5bb5ca64c7957582b46cd22d3ed0b188631eaf9
jshlw <JulioHenrique31501234@hotmail.com>
2019-12-03 14:42:01 -0300

    Add SFX singleton

---
+++ scripts/SFX.gd
@@ -0,0 +1,49 @@
+extends ResourcePreloader
+
+
+var max_sounds = 8
+
+
+func _ready():
+	Events.connect("play_audio", self, "play")
+
+
+func play(path, bus = "Master", volume = 1, ignore_same = false):
+	var sound = Sound.new()
+	sound.name = path.get_file()
+	
+	sound.volume_db = linear2db(volume)
+	
+	if ignore_same:
+		if !has_node(sound.name) && get_child_count() < max_sounds:
+			add_child(sound)
+		else:
+			sound = null
+			return
+	else:
+		if get_child_count() < max_sounds:
+			add_child(sound)
+		else:
+			sound = null
+			return
+	
+	var res
+	
+	if has_resource(path):
+		res = get_resource(path)
+	else:
+		res = load(path)
+		add_resource(path, res)
+	
+	sound.stream = res
+	
+	sound.play()
+
+
+class Sound extends AudioStreamPlayer:
+	func _init():
+		connect("finished", self, "_on_finished")
+	
+	
+	func _on_finished() -> void:
+		queue_free()
