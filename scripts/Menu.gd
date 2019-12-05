4f1c6b0fda944ed343c7e1d8879fb96dba4ab7d5
jshlw <JulioHenrique31501234@hotmail.com>
2019-12-05 11:12:34 -0300

    Translate labels

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -1,5 +1,14 @@
 extends Control
 
 
+onready var NewGame = $HBox/NewGame
+onready var Quit = $HBox/Quit
+
+
 func _ready():
-	pass
+	_translate()
+
+
+func _translate():
+	NewGame.text = tr("LABEL_NEWGAME")
+	Quit.text = tr("LABEL_QUIT")
