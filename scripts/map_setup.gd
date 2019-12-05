b9f8ead8b8b9065ada3139781bf724c23c61a828
jshlw <JulioHenrique31501234@hotmail.com>
2019-12-05 19:56:41 -0300

    Remove useless scripts

--- scripts/map_setup.gd
+++
@@ -1,2 +0,0 @@
-var hud
-var map
--- scripts/settings.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-func get_screen_middle():
-	var x = (OS.window_width * 0.5) - (OS.window_width * 0.5)
-	var y = (OS.window_height * 0.5) - (OS.window_height * 0.5)
-
-	return Vector2(x, y)
