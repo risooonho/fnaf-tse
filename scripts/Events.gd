fd47911048beac49ef121f7cc2c3ce10f63a01e3
jshlw <JulioHenrique31501234@hotmail.com>
2019-12-05 11:18:08 -0300

    Add menu signals

--- scripts/Events.gd
+++ scripts/Events.gd
@@ -6,5 +6,10 @@ signal game_started
 signal game_finished
 
 
+# Menu
+signal new_game_button_started(newgame)
+signal quit_button_started(quit)
+
+
 # Background loading
 signal loading_requested(scene_path)
