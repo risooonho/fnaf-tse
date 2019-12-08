16287a36acf511c839e76f030cbe082c5eea6ddc
jshlw <JulioHenrique31501234@hotmail.com>
2019-12-08 11:14:00 -0300

    Update Menu.gd

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -1,10 +1,6 @@
 extends Node
 
 
-enum STATES {IDLE, BUSY}
-var state = STATES.IDLE
-
-
 onready var NewGame = $GUI/Control/NewGame
 onready var Quit = $GUI/Control/Quit
 
@@ -14,18 +10,16 @@ func _ready():
 
 
 func _on_NewGame_pressed():
-	if state == STATES.IDLE:
-		Events.emit_signal("loading_requested", "res://scenes/map/Map.tscn")
+	Events.emit_signal("loading_requested", "res://scenes/map/Map.tscn")
 
 
 func _on_Quit_pressed():
-	if state == STATES.IDLE:
-		Events.emit_signal("selector_quit")
+	Events.emit_signal("selector_quit")
 		
-		var timer = get_tree().create_timer(0.5)
-		yield(timer, "timeout")
-		
-		get_tree().quit()
+	var timer = get_tree().create_timer(0.5)
+	yield(timer, "timeout")
+	
+	get_tree().quit()
 
 
 func _on_NewGame_mouse_entered():
