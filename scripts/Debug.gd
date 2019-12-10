8bf4af86390ed9871da94e92a6395cef0a5ba2cb
jshlw <JulioHenrique31501234@hotmail.com>
2019-12-10 10:58:57 -0300

    Get hardware info

--- scripts/Debug.gd
+++ scripts/Debug.gd
@@ -12,14 +12,16 @@ onready var Text = $Text
 
 
 func _ready():
+	dict.CORES[0] = OS.get_processor_count()
+	
 	refresh()
 
 
 func refresh():
 	dict.FPS[0] = Performance.get_monitor(Performance.TIME_FPS)
-	dict.RAM[0] = Performance.get_monitor(Performance.MEMORY_STATIC) + Performance.get_monitor(Performance.MEMORY_DYNAMIC)
+	dict.RAM[0] = stepify(Performance.get_monitor(Performance.MEMORY_STATIC) / 1000000, 0.01)
 	
-	if dict.FPS[0] <= 15:
+	if dict.FPS[0] <= 20:
 		dict.FPS[1] = Color.red.to_html(false)
 	else:
 		dict.FPS[1] = Color.white.to_html(false)
@@ -33,8 +35,8 @@ func refresh():
 
 
 func render():
-	var pattern = "[color=%s]%s: %s[/color]\n[color=%s]%s: %s\n[/color]"
-	Text.bbcode_text = pattern % [dict.FPS[1], str(dict.FPS[0]), str(dict.RAM), str(dict.CORES)]
+	var pattern = "[color=#%s]FPS: %s[/color]\n[color=#%s]RAM: %s MB[/color]\nCORES: %s"
+	Text.bbcode_text = pattern % [dict.FPS[1], str(dict.FPS[0]), str(dict.RAM[1]), str(dict.RAM[0]), str(dict.CORES[0])]
 
 
 func _on_Refresh_timeout():
