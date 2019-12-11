213b3465bb23987fdccf654c97893f7cc0ede8b0
jshlw <JulioHenrique31501234@hotmail.com>
2019-12-11 13:31:04 -0300

    Add Music system

---
+++ scripts/Music.gd
@@ -0,0 +1,8 @@
+extends Node
+
+
+const MUSIC_STREAM = preload("res://scenes/MusicStream.tscn")
+
+
+func play():
+	pass
---
+++ scripts/MusicStream.gd
@@ -0,0 +1,8 @@
+extends Node
+
+
+var resource_path
+
+
+func _ready():
+	pass
