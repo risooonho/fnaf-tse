12350f5ee77a2fb71f1d2bba38472b4a1ec77600
jshlw <JulioHenrique31501234@hotmail.com>
2019-12-12 18:25:59 -0300

    Add DialogueMessge flow control

--- scripts/gui/GameGUI.gd
+++ scripts/gui/GameGUI.gd
@@ -5,6 +5,7 @@ var message_object
 
 var DialogueMessage = preload("res://scenes/DialogueMessage.tscn")
 var Message
+var finished_message = false
 
 
 func _ready():
@@ -15,12 +16,14 @@ func _ready():
 
 
 func _on_message_object_requested(object):
+	# Initialize
 	Events.disconnect("message_object_requested", self, "_on_message_object_requested")
-	
+	set_process_unhandled_input(true)
 	get_tree().paused = true
-	
+	finished_message = false
 	message_object = object
 	
+	# Instance a DialogueMessage
 	Message = DialogueMessage.instance()
 	add_child(Message)
 	
@@ -28,8 +31,9 @@ func _on_message_object_requested(object):
 	Message.buff_text(message_object.message, 0.1)
 	Message.set_state(Message.STATE_OUTPUT)
 	
+	# Wait to finish
 	yield(Message, "buff_end")
-	set_process_unhandled_input(true)
+	finished_message = true
 	Events.connect("message_object_requested", self, "_on_message_object_requested")
 
 
@@ -41,5 +45,11 @@ func _on_message_object_accepted(object):
 
 func _unhandled_input(event):
 	if event.is_action_pressed("use"):
-		get_tree().set_input_as_handled()
-		Events.emit_signal("message_object_accepted", message_object)
+		if finished_message:
+			get_tree().set_input_as_handled()
+			Events.emit_signal("message_object_accepted", message_object)
+		else:
+			Message.set_buff_speed(0.02)
+	elif event.is_action_released("use"):
+		if !finished_message:
+			Message.set_buff_speed(0.1)
